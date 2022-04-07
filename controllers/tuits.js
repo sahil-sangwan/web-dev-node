02937919fc1e439a47c0745a52a35606114e7cb8
Sahil Sangwan <sah.sangwan@gmail.com>
2022-04-06 22:18:24 -0400

    first commit

---
+++ controllers/tuits.js
@@ -0,0 +1,34 @@
+export default [
+    {
+        "_id": "123",
+        "postedBy": {"username": "NASA"},
+        "tuit": "In 2021, our @NASAPersevere Mars rover landed",
+        "liked": true,
+        "verified": false,
+        "handle": "NASA",
+        "time": "2h",
+        "likes": 0,
+        "dislikes": 0,
+        "title": "Rover Lands on Mars",
+        "attachments": {
+            "video": "unKvMC3Y1kI"
+        },
+        "stats": {
+            "comments": 123,
+            "retuits": 234,
+            "likes": 345
+        }
+    },
+    {
+        "_id": "234",
+        "postedBy": { "username": "NASA" },
+        "tuit": "Ingenuity helicopter takes flight.",
+        "likes": 346785,
+        "dislikes": 0,
+        "stats": {
+            "comments": 123,
+            "retuits": 234,
+            "likes": 345
+        }
+    }
+];
